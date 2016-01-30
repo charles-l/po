703aaee665abb7d9395266c924f5c1481859a2a2
charlie <chuckiels2011@gmail.com>
2016-01-29 21:50:11 -0500

    began rewrite

--- lsp.c
+++ lsp.c
@@ -5,320 +5,195 @@
 #include <string.h>
 #include <assert.h>
 
-#define STACK_SIZE 1024
-#define FIRST_ATOM (s->atoms + 1)
+// quote, atom, eq, car, cdr, cons, and cond
 
 #define P(a) print_atom(a); printf("\n");
-#define error(args...) do { fprintf(stderr, args); exit(1); } while (0)
 
-// TODO: MAKE THE ENTIRE STACK AN ALIST THAT IT CAN REWRITE AT RUNTIME!!!
-// TODO: JIT for blazing fast speedz
-// TODO: replace labels with offset addresses
-// TODO: error type
-
-// I'm pretty sure this can all eventually be simplified to:
-// car
-// cdr
-// cons
-// null?
-// eq?
-// an environment list
-// stack
-
-typedef enum opcode { NOP   = 0x0,
-    PUSHA = 0x1, // push atom
-    PUSHN = 0x2, // push number
-    PUSHC = 0x3, // push cons
-    PUSH0 = 0x5, // push null
-    POP   = 0x6,
-
-    CAR   = 0xA,
-    CDR   = 0xB,
-
-    QUOTE = 0xC,
-    ATOM  = 0xD,
-    EQ    = 0xE,
-    COND  = 0xF,
-
-    ///
-
-    FFI   = 0x40, // FFI call
-    EOP   = 0x7f // end of program
-} opcode;
-
-typedef enum a_type {
-    SYM,
-    NUM,
-    CONS
-} a_type;
+enum type {CONS, ATOM, LAMBDA, FFI};
 
 typedef struct atom {
-    a_type type;
+    enum type type;
     union {
-        struct atom * (*func)(struct atom *a);
-        int num;
         char *sym;
+        struct atom *(*func)(struct atom *, struct atom *);
         struct {
-            struct atom *car, *cdr;
+            struct atom *car;
+            struct atom *cdr;
         };
     };
-} atom_t;
+} atom;
 
-typedef struct state {
-    atom_t **atoms;
-    atom_t **sp;
-    size_t size;
-} state_t;
-
-atom_t *nil;
-
-state_t init_state() {
-    state_t s;
-    s.size = STACK_SIZE;
-    s.atoms = malloc(sizeof(atom_t *) * s.size);
-    s.sp = s.atoms;
-
-    return s;
-}
-
-int isnil(atom_t *t) {
-    if (t == NULL) return 1;
-    return t->car == NULL && t->cdr == NULL;
-}
-
-void destroy_atom(atom_t *a, int fulldestroy) {
-    if(isnil(a) || a == NULL) return;
+void print_atom(atom *a) {
+    if(a == NULL) {printf("null"); return;}
     switch(a->type) {
-        case NUM:
-            break;
-        case SYM:
-            free(a->sym); // free malloced string
+        case ATOM:
+            printf("%s", a->sym);
             break;
         case CONS:
-            // fulldestroy will destroy all cons
-            if(fulldestroy) {
-                destroy_atom(a->car, 1);
-                if(a->cdr != nil) {
-                    destroy_atom(a->cdr, 1);
-                }
-            }
+            printf("(");
+            print_atom(a->car);
+            printf(" ");
+            print_atom(a->cdr);
+            printf(")");
+            break;
+        case LAMBDA:
+            printf("<l: %p>", a);
+            break;
+        case FFI:
+            printf("<ffi: %p>", a->func);
             break;
     }
-    free(a);
-    a = nil;
 }
 
-void destroy_state(state_t *s) {
-    atom_t **i = FIRST_ATOM;
-    while(i <= s->sp)
-    {
-        destroy_atom(*(i++), 1);
-    }
-    free(s->atoms);
-}
+const atom nil = {.type = ATOM, .car = NULL, .cdr = NULL};
+const atom tee = {.type = ATOM, .sym = "t"};
 
-void push(state_t *s, atom_t *v) {
-    s->sp++;
-    *(s->sp) = v;
-}
-
-atom_t *pop(state_t *s) {
-    return *(s->sp--);
-}
-
-atom_t *adup(atom_t *a) {
-    atom_t *r = malloc(sizeof(atom_t));
-    memcpy(r, a, sizeof(atom_t));
+atom *adup(atom *a) {
+    atom *r = malloc(sizeof(atom));
+    memcpy(r, a, sizeof(atom));
     return r;
 }
 
-#define GAP " "
+atom *natom(char *sym) {
+    atom r = (atom){.type = ATOM, .sym = strdup(sym)};
+    return adup(&r);
+}
 
-void print_atom(atom_t *a) {
-    if(a == NULL) return;
-    switch(a->type) {
-        case SYM:
-            printf("'%s'", a->sym);
-            break;
-        case NUM:
-            printf("%i", a->num);
-            break;
+// as opposed to `cons`, this func is used internally
+atom *ncons(atom *a, atom *b) {
+    atom r = (atom){.type = CONS, .car = a, .cdr = b};
+    return adup(&r);
+}
+
+atom *nffi(atom *(*func)(atom *)) {
+    atom r = (atom){.type = FFI, .func = func};
+    return adup(&r);
+}
+
+atom *nlambda(atom *args, atom *sexp) {
+    atom r = (atom){.type = LAMBDA, .car = args, .cdr = sexp};
+    return adup(&r);
+}
+
+atom *quote(atom *l, atom *env) {
+    return l;
+}
+
+atom *is_atom(atom *a, atom *env) {
+    if(a == NULL) return &nil;
+    if(a->type == ATOM) return &tee;
+    return &nil;
+}
+
+const atom *eq(atom *a, atom *env) {
+    if(a->car == NULL && a->cdr->car == NULL) return &tee;
+    if(a->car != a->cdr->car) return &nil;
+    switch(a->car->type) {
+        case ATOM:
+            if(strcmp(a->car->sym, a->cdr->car->sym) == 0) return &tee;
         case CONS:
-            if(isnil(a)) {
-                printf("nil");
-            } else {
-                printf("(", a);
-                print_atom(a->car);
-                printf(GAP);
-                print_atom(a->cdr);
-                printf(")");
+            if(eq(a->car, a->cdr->car)) {
+                return eq(a->cdr, a->cdr->cdr);
             }
-            break;
     }
 }
 
-void dump_state(state_t *s) {
-    puts("--- state dump ---");
-    for(atom_t **o = FIRST_ATOM; o <= s->sp; o++)
-    {
-        printf("%08p" GAP, *o);
-        P(*o);
-    }
-    puts("--- finished dump ---");
+atom *cons(atom *l, atom *env) {
+    return ncons(l->cdr, l->car);
 }
 
-// ignore \0
-char *istrchr(char *s, int c, int e) {
-    for(; s[0] != e; s++) {
-        if(s[0] == c) {
-            return s;
+int isnil(atom *a, atom *env) {
+    if(a->type == CONS) {
+        if (a->car == NULL) return 1;
+    }
+    return 0;
+}
+
+atom *eval(atom *sexp, atom *env);
+const atom *cond(atom *l, atom *env) {
+    if(l == NULL) return &nil;
+    if(l->cdr) {
+        if (eval(l->car, env)) return l->cdr;
+    }
+    while(l = l->cdr) {
+        if(l->car == &tee) {
+            return l->cdr;
         }
     }
+    return &nil;
+}
+
+atom *car(atom *a, atom *env) {
+    return a->car;
+}
+
+atom *cdr(atom *a, atom *env) {
+    return a->cdr;
+}
+
+atom *lookup(atom *a, char *nm) {
+    if(a == NULL || a->car == NULL) return NULL;
+    if(a->car->car->type == ATOM && strcmp(a->car->car->sym, nm) == 0) {
+        return a->car->cdr;
+    }
+    if(a->cdr != NULL)
+        return lookup(a->cdr, nm);
     return NULL;
 }
 
-char *gettok(char **c) {
-    char *tmp = istrchr(*c, '\x0', '\x7f');
-    size_t i = (size_t) (tmp - *c);
-    char *r = malloc(i);
-    memcpy(r, (*c) + 1, i);
-    *c = tmp;
-    return r;
+atom *append(atom *l, atom *a) {
+    return ncons(a, l);
 }
 
-// note: r MUST be allocated memory
-atom_t *sym (char *r) {
-    atom_t a = {.type = SYM, .sym = r};
-    return adup(&a);
+atom *eval_fn(atom *sexp, atom *env) {
+    atom *s = sexp->car;
+    atom *a = sexp->cdr;
+    if(s->type == LAMBDA) {
+        return nlambda(sexp, env);
+    } else if(s->type == FFI){
+        P(s);
+        return(((atom *) s)->func)(a, env);
+    }
 }
 
-atom_t *num(int v) {
-    atom_t a = {.type = NUM, .num = v};
-    return adup(&a);
-}
+atom *eval(atom *sexp, atom *env) {
+    if(sexp == NULL)
+        return;
 
-atom_t *cons(atom_t *x, atom_t *y) {
-    atom_t a = {.type = CONS, .car = adup(x), .cdr = adup(y)};
-    // TODO: figure this out
-    //destroy_atom(x, 0);
-    //destroy_atom(y, 0);
-    return adup(&a);
-}
+    if(sexp->type == CONS) {
+        if(sexp->car->type == ATOM && strcmp(sexp->car->sym, "lam") == 0) {
+            return nlambda(sexp->car->cdr, sexp->cdr->cdr->car);
+        } else {
+            atom *a = ncons(eval(sexp->car, env), NULL);
+            sexp = sexp->cdr;
 
-atom_t *car(atom_t *a) {
-    if(a->type == CONS)
-        return a->car;
-    return a;
-}
-
-atom_t *cdr(atom_t *a) {
-    if(a->type == CONS)
-        return a->cdr;
-    return a;
-}
-
-atom_t *eq(atom_t *a, atom_t *b) {
-    if(a->type != b->type) return nil;
-    switch(a->type) {
-        case NUM:
-            if(a->num == b->num) return sym(strdup("t"));
-        case SYM:
-            if(strcmp(a->sym, b->sym) == 0) return sym(strdup("t"));
-        case CONS:
-            if(eq(a->car, b->car)) {
-                return eq(a->cdr, b->cdr);
-            } else {
-                return nil;
+            while (sexp != NULL && sexp->type == CONS){
+                append(a, eval(sexp->car,env));
+                sexp = sexp->cdr;
             }
-    }
-    return nil;
-}
 
-atom_t *isatom(atom_t *a) {
-    if(a->type == SYM || a->type == NUM) {
-        return sym(strdup("t"));
+            return eval_fn(a, env);
+        }
     } else {
-        return nil;
+        atom *v = lookup(env, sexp->sym);
+        if(v == NULL) {
+            return sexp;
+        } else {
+            return v;
+        }
     }
-}
 
-#define CASE(en, b) \
-    case en: \
-b; \
-break;
-
-void eval(state_t *s, char **c) {
-    char *t;
-    atom_t *a;
-    atom_t *x;
-    atom_t *y;
-    switch((*c)[0]) {
-        // TODO: do not reference prog directly
-        CASE(NOP, NULL);
-        CASE(PUSHA, push(s, sym(gettok(c))));
-        CASE(PUSHN,
-                t = gettok(c);
-                push(s, num(atoi(t)));
-                free(t));
-        CASE(PUSHC,
-                x = pop(s);
-                y = pop(s);
-                push(s, cons(x, y)));
-        CASE(PUSH0,
-                push(s, nil));
-        CASE(CAR,   push(s, adup(car(*(s->sp)))));
-        CASE(CDR,   push(s, adup(cdr(*(s->sp)))));
-        CASE(POP,   a = pop(s); destroy_atom (a, 1));
-        CASE(QUOTE, push(s, cdr(*(s->sp))));
-        CASE(ATOM,  push(s, isatom(*(s->sp))));
-        //CASE(COND,  push(s, );
-                default:
-                error("UNKNOWN OPCODE: 0x%x\n", (*c)[0]);
-    }
-    (*c)++;
-}
-
-void run(state_t *s, char *prog) {
-    char *c = prog;
-    atom_t *env = nil;
-    while(c[0] != EOP) {
-        eval(s, &c);
-    }
 }
 
 int main(void) {
-    // set nil
-    atom_t a = {.type = CONS, .car = NULL, .cdr = NULL};
-    nil = &a;
-
-    state_t s = init_state();
-    char *prog =
-        // (def a 312)
-        "\x5"           // push nil
-        "\x2" "312\x0"  // push num
-        "\x3"           // push cons
-        "\x1" "a\x0"    // push sym
-        "\x3"           // push cons
-        "\x1" "def\x0"  // push sym
-        "\x3"           // push cons
-
-        // a
-        "\x1" "a\x0"
-
-        // (if 'a 'a 'b)
-        "\x5"
-        "\x1" "b\x0"
-        "\x3"
-        "\x1" "a\x0"
-        "\x3"
-        "\x5"
-        "\x3"
-        "\x1" "if\x0"
-        "\x3"
-        "\x7f";
-
-    run(&s, prog);
-    dump_state(&s);
-    destroy_state(&s);
-    return 0;
+    atom *env = ncons(ncons(natom("quote"),nffi(&quote)), NULL);
+    env = append(env, ncons(natom("atom?"), nffi(&is_atom)));
+    env = append(env, ncons(natom("eq?"), nffi(&eq)));
+    env = append(env, ncons(natom("car"), nffi(&car)));
+    env = append(env, ncons(natom("cdr"), nffi(&cdr)));
+    env = append(env, ncons(natom("cons"), nffi(&cons)));
+    env = append(env, ncons(natom("cond"), nffi(&cond)));
+    P(env);
+    P(ncons(natom("atom?"), ncons(natom("a"), NULL)));
+    P(eval(ncons(natom("atom?"), ncons(natom("a"), NULL)), env));
 }
