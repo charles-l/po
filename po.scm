db2ee515e04e932bb0e5da9909d36f8f47c211ed
nc <chuckiels2011@gmail.com>
2016-12-04 16:41:09 -0500

    cleanup

--- po.scm
+++ po.scm
@@ -69,25 +69,31 @@
 (define (emit . args)
   (set! main-asm (append main-asm `(,args))))
 
+; (emit* ('a 'b 'c) ('d e f)) eq to (emit 'a 'b 'c) (emit 'd e f)
+(define-syntax emit*
+  (lambda (exp rename compare)
+    (append `(begin)
+	    (map (lambda (x) (cons 'emit x)) (cdr exp)))))
+
 (define (emit-immediate e)
   (emit 'movl (immediate-rep e) '%eax))
 
 (define (emit-cmp-eax val) ; cmp eax to val (TODO: make this more efficient)
-  (emit 'movl (immediate-rep #f) '%ecx) ; move #t and #f into registers
-  (emit 'movl (immediate-rep #t) '%edx)
-  (emit 'cmpl val '%eax)
-  (emit 'cmovzl '%edx '%ecx)
-  (emit 'movl '%ecx '%eax)) ; move the result to %eax
+  (emit* ('movl (immediate-rep #f) '%ecx) ; move #t and #f into registers
+	 ('movl (immediate-rep #t) '%edx)
+	 ('cmpl val '%eax)
+	 ('cmovzl '%edx '%ecx)
+	 ('movl '%ecx '%eax))) ; move the result to %eax
 
 (define (emit-new-heap-val size tag initials)
   (mapi (lambda (e i)
 	  (emit 'movl e (conc (* i word-size) "(%esi)")))
 	initials)
-  (emit 'movl '%esi  '%eax)
-  (emit 'orl  tag    '%eax)
-  (emit 'addl size   '%esi) ; advance %esi
-  (emit 'addl ($ 11) '%esi) ; and align it on 8 bytes
-  (emit 'andl ($ -8) '%esi))
+  (emit* ('movl '%esi  '%eax)
+	 ('orl  tag    '%eax)
+	 ('addl size   '%esi) ; advance %esi
+	 ('addl ($ 11) '%esi) ; and align it on 8 bytes
+	 ('andl ($ -8) '%esi)))
 
 (define (emit-push-all-to-stack l si env) ; push a list to the stack
   (if (not (null? l))
@@ -111,11 +117,11 @@
   (emit 'andl mask '%eax))
 
 (define (emit-function-header name)
-  (emit '.text)
-  (emit '.global name)
-  (emit '.type name '@function)
-  (emit '.align 8)
-  (emit (label name)))
+  (emit* ('.text)
+	 ('.global name)
+	 ('.type name '@function)
+	 ('.align 8)
+	 ((label name))))
 
 (define (emit-code fmls frees body si env)
   (let ((code-id (uniq-label 'L)))
@@ -133,6 +139,8 @@
 		(emit 'ret))
 	      main-asm))
     code-id))
+
+; assumes e, si, and env are bound
 
 (define (emit-primcall e si env)
   (case (car e)
@@ -167,6 +175,12 @@
     ((zero?)
      (emit-expr (cadr e) si env)
      (emit-cmp-eax ($ 0)))
+    ((car) ; TODO: check that type is a pair
+     (emit-expr (cadr e) si env)
+     (emit 'movl "-1(%eax)" '%eax))
+    ((cdr) ; TODO: check that type is a pair
+     (emit-expr (cadr e) si env)
+     (emit 'movl "3(%eax)" '%eax))
     ((eq?)
      (emit-expr (cadr e) si env)
      (emit 'mov '%eax '%ebx)
@@ -194,23 +208,17 @@
      (emit-expr (cadr e) si env) ; compile sub exprs first
      (emit-push-to-stack si) ; and push scratch to stack
      (emit-expr (caddr e) (- si word-size) env)
-     (emit 'movl '%eax "4(%esi)") ; second word of esi
-     (emit 'movl (stack-pos si) '%eax) ; move from stack to
-     (emit 'movl '%eax "0(%esi)") ; first word of esi
-     (emit 'movl '%esi '%eax)
-     (emit 'orl  ($ pair-tag) '%eax) ; mark as pair
-     (emit 'addl ($ double-word) '%esi)) ; bump esi forward
-    ((car) ; TODO: check that type is a pair
-     (emit-expr (cadr e) si env)
-     (emit 'movl "-1(%eax)" '%eax))
-    ((cdr) ; TODO: check that type is a pair
-     (emit-expr (cadr e) si env)
-     (emit 'movl "3(%eax)" '%eax))
+     (emit* ('movl '%eax "4(%esi)") ; second word of esi
+	    ('movl (stack-pos si) '%eax) ; move from stack to
+	    ('movl '%eax "0(%esi)") ; first word of esi
+	    ('movl '%esi '%eax)
+	    ('orl  ($ pair-tag) '%eax) ; mark as pair
+	    ('addl ($ double-word) '%esi))) ; bump esi forward
     ((make-vector) ; TODO: add vector-set and vector-ref and type check
      (emit-expr (cadr e) si env)
-     (emit 'shr ($ fixnum-shift) '%eax) ; no need for type data - we already know it's a uint
-     (emit 'imul ($ word-size) '%eax) ; each element is sizeof(word)
-     (emit 'movl '%eax '%ebx) ; backup %eax
+     (emit* ('shr ($ fixnum-shift) '%eax) ; no need for type data - we already know it's a uint
+	    ('imul ($ word-size) '%eax) ; each element is sizeof(word)
+	    ('movl '%eax '%ebx)) ; backup %eax
      (emit-new-heap-val '%ebx ($ vector-tag) '(%ebx)))
     ((vector-length)
      (emit-expr (cadr e) si env)
@@ -232,18 +240,18 @@
      (emit 'movl '%eax '%ebx) ; index
      (emit 'shr  ($ fixnum-shift) '%ebx)
      (emit-expr (cadddr e) si env) ; char
-     (emit 'addl '%ebx '%ecx)
-     (emit 'shr  ($ char-shift) '%eax)
-     (emit 'movb '%al (string-append (->string (- (- string-tag 4))) "(%ecx)")) ; TODO: fix warning for this
-     (emit 'movl '%edx '%eax))
+     (emit* ('addl '%ebx '%ecx)
+	    ('shr  ($ char-shift) '%eax)
+	    ('movb '%al (conc (- (- string-tag 4)) "(%ecx)")) ; TODO: fix warning for this
+	    ('movl '%edx '%eax)))
     ((string-ref)
      (emit-expr (cadr e) si env) ; string
      (emit 'movl '%eax '%ecx)
      (emit-expr (caddr e) si env) ; index
-     (emit 'shr  ($ fixnum-shift) '%eax)
-     (emit 'addl '%eax '%ecx)
-     (emit 'movb (string-append (->string (- (- string-tag 4))) "(%ecx)") '%ah)
-     (emit 'orl  ($ char-tag) '%eax))
+     (emit* ('shr  ($ fixnum-shift) '%eax)
+	    ('addl '%eax '%ecx)
+	    ('movb (conc (- (- string-tag 4)) "(%ecx)") '%ah)
+	    ('orl  ($ char-tag) '%eax)))
     ((labels)
      (emit-expr (caddr e)
 		si
@@ -272,22 +280,17 @@
 (define (emit-funcall e si env)
   (emit-push-all-to-stack (cdr e) (- si word-size) env) ; save the args
   (emit-expr (car e) si env)
-  (emit 'addl ($ (+ si word-size)) ; neg size of current stack (and ret addr)
-	'%esp)
-  (emit 'subl ($ closure-tag) '%eax)
-  (emit 'addl ($ word-size) '%eax)
-  (emit 'call '*\(%eax\))
-  (emit 'subl ($ (+ si word-size)) ; add back size of current stack (and ret addr)
-	'%esp))
+  (emit* ('addl ($ (+ si word-size)) '%esp) ; neg size of current stack (and ret addr)
+	 ('subl ($ closure-tag) '%eax)
+	 ('addl ($ word-size) '%eax)
+	 ('call '*\(%eax\))
+	 ('subl ($ (+ si word-size)) '%esp))) ; add back size of current stack (and ret addr)
 
 (define (emit-expr e si env)
   (cond ((immediate? e)
 	 (emit-immediate e))
 	((variable? e)
-	 (let ((v (lookup e env)))
-	   (if v
-	     (emit 'movl (stack-pos v) '%eax)
-	     (error "undefined binding" e))))
+	 (emit 'movl (stack-pos (lookup! e env)) '%eax))
 	((let? e)
 	 (emit-let (cadr e) (caddr e) si env))
 	((funcall? e)
@@ -322,6 +325,12 @@
 
 (define (lookup var env) ; returns the stack offset of a variable in env
   (cadr (or (assoc var env) '(#f #f))))
+
+(define (lookup! var env)
+  (let ((e (lookup var env)))
+    (if e
+      e
+      (error "undefined binding" var))))
 
 (define (push-var var si env)
   `((,var ,si) . ,env))
