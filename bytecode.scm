fe4a7cab90907852a5a55c4690e453dd4de35177
nc <charles.saternos@gmail.com>
2017-03-14 15:56:28 -0400

    added pairs and section info

--- bytecode.scm
+++ bytecode.scm
@@ -1,25 +1,23 @@
-(use srfi-13)
+(use srfi-13 srfi-69)
 (define *val*  'eax)
 (define *env*  'ebx)
 (define *fun*  'ecx)
-; TODO: write internal heap manager
 (define *t1*   'edx)
 (define word-size 4)
 
-(define env.init '(()))
-
 (define num-tag  #b000)
 (define nil-tag  #b001)
-(define true-tag #b010)
-
-(define cons-tag #b011)
-(define str-tag  #b100)
-(define sym-tag  #b101)
+(define cons-tag #b010)
+(define str-tag  #b011)
+(define sym-tag  #b100)
+(define vec-tag  #b101)
 (define fun-tag  #b110)
-(define vec-tag  #b111)
 
 (define (tag v tag)
   (bitwise-ior (arithmetic-shift v 3) tag))
+
+(define (deref . e)
+  (symbol-append '\[ (string->symbol (string-join (map ->string e))) '\]))
 
 (define (untag v)
   (arithmetic-shift v -3))
@@ -28,9 +26,12 @@
   (cond
     ((number? v) (tag v num-tag))
     ((null? v) (tag v nil-tag))
-    ((eq? #t v) (tag v true-tag))))
+    ;((eq? #t v) (tag v true-tag))
+    ))
 
 ;;;
+
+(define env.init '(()))
 
 (define (env-push env k)
   (cons (cons k (car env)) (cdr env)))
@@ -44,13 +45,21 @@
 (define (env-lookup env k)
   (list-index (cut eq? <> k) (car env)))
 
+(define global-vars (make-hash-table))
+(define (global-ref i)
+  (if (hash-table-exists? global-vars i)
+    (hash-table-ref global-vars i)
+    (let ((s (gensym i)))
+      (hash-table-set! global-vars i s)
+      s)))
+
 (define (CONSTANT v)
   `(mov ,*val* ,(tagged-val v)))
 
-; TODO:
-;(define (GLOBAL-SET! i m)
-;  (delay (m)
-;         (global-update! i *val*)))
+(define (GLOBAL-SET! i m)
+  (let ((sym (global-ref i)))
+    `(,m
+       (mov ,(deref sym) ,*val*))))
 
 (define (SEQUENCE m m+)
   (append (m)
@@ -77,27 +86,13 @@
           ))
 
 (define (REGULAR-CALL m m*)
-  (append m (PUSH-VAL)
-          m* (POP-FUNCTION) (PRESERVE-ENV)
-          (FUNCTION-INVOKE) (RESTORE-ENV)))
-
-(define (PUSH-VAL)
-  `(push ,*val*))
-
-(define (POP-VAL)
-  `(pop ,*val*))
-
-(define (POP-FUNCTION)
-  `(pop ,*fun*))
-
-(define (PRESERVE-ENV)
-  `(push ,*env*))
-
-(define (FUNCTION-INVOKE)
-  `(call ,*fun*))
-
-(define (RESTORE-ENV)
-  `(pop ,*env*))
+  `(,m
+     (push ,*val*)
+     ,m*
+     (pop ,*fun*)
+     (push ,*env*)
+     (call ,*fun*)
+     (pop ,*env*)))
 
 (define (JMP l)
   `(jmp ,l))
@@ -110,17 +105,27 @@
   `(,@(MALLOC (* 2 word-size))
     (mov ,*t1* ,*val*)
     ,a
-    (mov (,*t1*) ,*val*)
-    ;`(mov ,(+ addr word-size) ,b) ; TODO: pack b in lower portion
-    ))
+    (mov ,(deref *t1*) ,*val*)
+    ,b
+    (mov ,(deref *t1* '+ word-size) ,*val*)
+    (mov ,*val* ,*t1*)
+    (or  ,*val* ,cons-tag)))
 
 (define (CMP-NIL r)
   ; TODO: cmp nil tag against reg r
   '())
 
 (define (JMP-FALSE l)
-  (CMP-NIL *val*)
-  `(je ,l))
+  `(,@(CMP-NIL *val*)
+     (je ,l)))
+
+(define (CAR v)
+  ; TODO: assert type
+  `((mov ,v ,(deref v '- cons-tag))))
+
+(define (CDR v)
+  ; TODO: assert type
+  `((mov ,v ,(deref v '+ (- word-size cons-tag)))))
 
 (define (ALTERNATIVE con t e)
   (let ((le (gensym 'else)) (ld (gensym 'd)))
@@ -139,11 +144,21 @@
      (display " ")
      (print (string-join (map ->string (cdr i)) ",")))))
 
-(print-instr '("global _start"))
-(print-instr '("extern malloc"))
-(print-instr '("extern exit"))
+(print "section .text")
+(print "global _start")
+(print "extern malloc")
+(print "extern exit")
 (print-instr "_start")
-(map print-instr (CONS (CONSTANT 1) (CONSTANT 2)))
+(map print-instr
+     (append
+       (GLOBAL-SET! 'a (CONSTANT 3))
+       (CONS (CONSTANT 1) (CONSTANT 2))
+       (CAR *val*)))
 
-(print-instr `(push 0))
+(print-instr '(push 0))
 (print-instr '(call exit))
+
+(print "section .data")
+(for-each (lambda (l)
+            (print (conc (hash-table-ref global-vars l) " " 'dw " " 0)))
+          (hash-table-keys global-vars))
